c8e374316860336bfdca4597b3fa35bb9aafd9d2
dotnetCarpenter <jon.ronnenberg@gmail.com>
2012-10-15 20:50:40 +0200

    remove '.tmp' from file names

---
+++ removeDottmpFromFiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @version 12.10.1
+htmlFiles=(`ls | grep ".tmp"`)	# grap all files with ".tmp" in file name in current directory
+for file in "${htmlFiles[@]}"	# loop through all files
+do
+	`mv $file ${file/%\.tmp/}`	# exucute mv command with file name and file name with-out ".tmp"
+	#echo ${file/%\.tmp/}		# debugging line - ignore
+done
+exit							# exit with status code from last command (mv)

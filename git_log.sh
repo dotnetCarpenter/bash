29a34bbbb4eadb85bcff0394f0945954474c6730
dotnetCarpenter <jon.ronnenberg@gmail.com>
2012-10-16 17:30:50 +0200

    added usage info, verbose command and version command

--- git_log.sh
+++ git_log.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
-# @version 12.10.3
-gitpath=$1; filename=$2
+# git_log.sh
+# @version 12.10.4
+version="12.10.4"
+gitpath=$1; filename=$2; verbose=false
+
+if [[ "$gitpath" == "-h" || "$gitpath" == "--help" ]]; then
+	echo "Usage: $0 [-v|--version] | [--verbose] [gitpath] [filename]"
+	echo "Where gitpath is a path to the git repository you want to get commits from."
+	echo "filename is the name of the file you want to save the commits in. Example: ~/git_logs/myProject_log.tsv"
+	echo
+	echo "git log --since=\"Jun 1 2012\" --author=dotnet[Cc]arpenter --date=iso --reverse --pretty=\"%ad	%s\""
+	echo "$0 is interactive if gitpath and filename is not provided."
+	exit 0
+fi
+
+if [ "$gitpath"=="--verbose" ]; then
+	verbose=true
+	gitpath=$2; filename=$3
+fi
+
+if [[ "$gitpath"=="-v" || "$gitpath"=="--version" ]]; then
+	echo $version
+	echo "(year.month.revision)"
+	exit 0
+fi
 
 if [ -z "$gitpath" ]
 then
@@ -13,11 +36,11 @@ if [ -z "$filename" ]
 then
 	read -p "Enter file name/path to save your log in (git_log.tsv): " $filename
 fi
-filename=${filename:-"git_log.tsv"}										# set gitpath to default if empty
+filename=${filename:-"git_log.tsv"}										# set filename to default if empty
 
 . changedir.sh															# import changedir.sh
 changeDir $gitpath
-echo "Changed directory to $PWD"
+$verbose && echo "Changed directory to $PWD"
 
 #echo $gitpath															# debugging line - ignore
 #echo $filename															# debugging line - ignore
